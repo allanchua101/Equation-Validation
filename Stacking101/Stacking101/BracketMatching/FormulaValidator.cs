3cf830a1e044592a0d79f251a0eb638a455a9bab
allanchua101 <allanchua.officefiles@gmail.com>
2017-03-23 21:34:59 +0800

    Initial Commit

---
+++ Stacking101/Stacking101/BracketMatching/FormulaValidator.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Stacking101.BracketMatching
+{
+    public class FormulaValidator
+    {
+        // Question: Check if a string is balanced. Every opening bracket is matched by a closing bracket in a correct position.
+        // { [ ( } ] )
+
+        // Example: "()" is balanced
+        // Example: "{ ]" is not balanced.
+        // Examples: "()[]{}" is balanced.
+        // "{([])}" is balanced
+        // "{ ( [ ) ] }" is _not_ balanced
+
+        // Input: string, containing the bracket symbols only
+        // Output: true or false
+        public bool IsBalanced(string input)
+        {
+            var brackets = BuildBracketMap();
+            var openingBraces = new Stack<char>();
+            var inputCharacters = input.ToCharArray();
+
+            foreach (char character in inputCharacters)
+            {
+                if (brackets.ContainsKey(character))
+                {
+                    openingBraces.Push(character);
+                }
+
+                if (brackets.ContainsValue(character))
+                {
+                    var closingBracket = character;
+                    var openingBracket = brackets.FirstOrDefault(x => x.Value == closingBracket).Key;
+
+                    if (openingBraces.Peek() == openingBracket)
+                        openingBraces.Pop();
+                    else
+                        return false;
+                }
+            }
+
+            return openingBraces.Count == 0;
+        }
+
+        private Dictionary<char, char> BuildBracketMap()
+        {
+            return new Dictionary<char, char>()
+            {
+                {'[', ']'},
+                {'(', ')'},
+                {'{', '}'}
+            };
+        }
+    }
+}
---
+++ Stacking101/Stacking101/Program.cs
@@ -0,0 +1,30 @@
+﻿using Stacking101.BracketMatching;
+using System;
+
+namespace Stacking101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testData = new string[] {
+                "(x + y) - ((z * y) + x)",
+                "{(1 / 2) + (y / z)} - ([g + z]) - (x + y)",
+                "[x + y] - (5 * x)",
+                "(x + 3]",
+                "{(y + z)]",
+                "(x + y) + [g(5])]"
+            };
+            var formulaValidator = new FormulaValidator();
+
+            foreach(var equation in testData)
+            {
+                Console.WriteLine(
+                    string.Format("{0} {1}", equation, formulaValidator.IsBalanced(equation))
+                );
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
